1cf076a77e21aed5e0f37d2334a1ac682b146a24
Ali Dehghani <dehghanialif@gmail.com>
2024-01-23 13:19:39 +0100

    Clean up the genres

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "b4218326e0064353a3330ed4625226d8",
+    key: "fa650e30ab6a4751a288875826d387e8",
   },
 });
